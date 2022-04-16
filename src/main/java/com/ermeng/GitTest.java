fcb34f30f9c1a775a2804779792352027846e5a4
ermeng <2669498105@qq.com>
2022-04-17 00:23:44 +0800

    first commit

---
+++ src/main/java/com/ermeng/GitTest.java
@@ -0,0 +1,11 @@
+package com.ermeng;
+
+/**
+ * @author ermeng
+ * @create 2022-04-17 0:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
